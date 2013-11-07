44e7e416dd2d0049212859585b796c20c3dbc4b4
Duffy-Coissard Adrien <aduffycois@if501-219-18.insa-lyon.fr>
2013-11-07 18:28:14 +0100

    dd mtx struct

--- sem.h
+++ sem.h
@@ -9,13 +9,30 @@ struct waiting_process {
 
 struct sem_s {
     int val;
-
     int waiting;
     struct waiting_process* queue;
     struct waiting_process* last_process;
 };
 
+
+////////////mutex
+struct mtx_s {
+	int iIsLocked;
+	int iWaiting;
+	//pid_t pOwnerPid;
+	struct waiting_process* queue;
+	struct waiting_process* last_process;
+	
+};
+
 void sem_init(struct sem_s* sem, unsigned int val);
 void sem_down(struct sem_s* sem);
 void sem_up(struct sem_s* sem);
+
+
+void mtx_init(struct mtx_s* mutex);
+void mtx_lock(struct mtx_s* mutex);
+void mtx_unlock(struct mtx_s* mutex);
+
+
 #endif
